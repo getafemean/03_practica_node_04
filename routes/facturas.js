e11dc8234544204662a4ef29661b0eeca9e0ea38
getafemean <getafemean@gmail.com>
2021-05-26 19:12:08 +0200

    put

--- routes/facturas.js
+++ routes/facturas.js
@@ -34,4 +34,41 @@ app.post('/', (req, res) => {
     console.log(facturas);
 })
 
+app.put('/:id', (req, res) => {
+    if(req.body === undefined || JSON.stringify(req.body) === JSON.stringify({})) {
+        return res.status(400).json({
+            message: 'Datos de factura incorrectos'
+        })
+    }
+    let posicion = facturas.findIndex(elem => {
+        return elem.id === req.params.id;
+    })
+    if (posicion < 0) {
+        return res.status(404).json({
+            message: 'La factura no existe'
+        })
+    }
+    if(req.body.cliente !== undefined) {
+        facturas[posicion].cliente = req.body.cliente;
+    }
+    if(req.body.cif !== undefined) {
+        facturas[posicion].cif = req.body.cif;
+    }
+    if(req.body.fechaFactura !== undefined) {
+        facturas[posicion].fechaFactura = req.body.fechaFactura;
+    }
+    if(req.body.baseImponible !== undefined) {
+        facturas[posicion].baseImponible = req.body.baseImponible;
+    }
+    if(req.body.tipoIVA !== undefined) {
+        facturas[posicion].tipoIVA = req.body.tipoIVA;
+    }
+    if(req.body.user !== undefined) {
+        facturas[posicion].user = req.body.user;
+    }
+    res.status(201).json({
+        message: `La factura ha sido actualizada`
+    })
+})
+
 module.exports = app;
